0b422da51242bc2ff885b6fb75618a434bf1f2f7
Juan De Dios Santos <me@juandes.com>
2020-06-08 13:44:49 +1200

    Added OD model implementation

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
+// The image classifier
 let model;
+
+// The image classifier used for creating the activation maps
 let activationModel;
+
+// The object detection model
+let odModel;
 let imageTensor;
+let sourceImage;
+let ctx;
+
+const RADIX = 10;
+const BBCOLOR = '#008000';
+const ODSIZE = 500;
 
 
 async function drawActivation(activations, layerIndex, filterNumber, plotId, size) {
+  // Use tf.tidy to remove garbage collect the intermediate tensors.
   const activationToDraw = tf.tidy(() => {
-    // TODO: Choose the filter
-    const b = activations[layerIndex].slice([0, 0, 0, filterNumber], [1, size, size, 1]);
+    // Get the activation map of the given layer (layerIndex) and filter (filterNumber)
+    const activation = activations[layerIndex].slice([0, 0, 0, filterNumber], [1, size, size, 1]);
     // TypedArray to Array and reserve it on the axis #1.
-    const newB = Array.from(b.reverse(1).dataSync());
+    const activationArray = Array.from(activation.reverse(1).dataSync());
 
-    const newArr = [];
-    while (newB.length) newArr.push(newB.splice(0, size));
-    return newArr;
+    const reshapedActivation = [];
+    // Reshape array to 2D
+    while (activationArray.length) reshapedActivation.push(activationArray.splice(0, size));
+    return reshapedActivation;
   });
 
-  console.log(activationToDraw);
   const data = [
     {
       z: activationToDraw,
@@ -32,22 +45,91 @@ async function drawActivation(activations, layerIndex, filterNumber, plotId, siz
   Plotly.newPlot(plotId, data, layout);
 }
 
-async function init() {
-  initSliders('one', (value) => {
-    console.log(value);
-    const prediction = model.predict(imageTensor).dataSync();
-    const activations = activationModel.predict(imageTensor);
 
-    // model.layers[0].batchInputShape[1]
-    drawActivation(activations, 0, parseInt(value, 10), 'plot-activations-one', 150);
+function setODCanvas() {
+  const canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
+  sourceImage = document.getElementById('source');
+
+  canvas.width = ODSIZE;
+  canvas.height = ODSIZE;
+}
+
+const layersInformation = [
+  {
+    layer: 'one',
+    index: 0,
+    size: 150,
+  },
+  {
+    layer: 'two',
+    index: 1,
+    size: 75,
+  },
+  {
+    layer: 'three',
+    index: 2,
+    size: 75,
+  },
+  {
+    layer: 'four',
+    index: 3,
+    size: 37,
+  },
+  {
+    layer: 'five',
+    index: 4,
+    size: 37,
+  },
+  {
+    layer: 'six',
+    index: 5,
+    size: 18,
+  },
+];
+
+
+async function init() {
+  setODCanvas();
+
+  initSliders('one', (value) => {
+    const activations = activationModel.predict(imageTensor);
+    drawActivation(activations, 0, parseInt(value, RADIX), 'plot-activations-one', 150);
+  });
+
+  initSliders('two', (value) => {
+    const activations = activationModel.predict(imageTensor);
+    drawActivation(activations, 1, parseInt(value, RADIX), 'plot-activations-two', 75);
+  });
+
+  initSliders('three', (value) => {
+    const activations = activationModel.predict(imageTensor);
+    drawActivation(activations, 2, parseInt(value, RADIX), 'plot-activations-three', 75);
+  });
+
+  initSliders('four', (value) => {
+    const activations = activationModel.predict(imageTensor);
+    drawActivation(activations, 3, parseInt(value, RADIX), 'plot-activations-four', 37);
+  });
+
+  initSliders('five', (value) => {
+    const activations = activationModel.predict(imageTensor);
+    drawActivation(activations, 4, parseInt(value, RADIX), 'plot-activations-five', 37);
+  });
+
+  initSliders('six', (value) => {
+    const activations = activationModel.predict(imageTensor);
+    drawActivation(activations, 5, parseInt(value, RADIX), 'plot-activations-six', 18);
   });
 
 
   model = await tf.loadLayersModel('http://127.0.0.1:8080/model/tfjs-2/model.json');
   model.summary();
+  odModel = await tf.automl.loadObjectDetection('model/object-detection/model.json');
+  console.log(odModel);
   const outputLayers = [];
 
-  model.layers.slice(0, 6).forEach((layer, i) => {
+  model.layers.slice(0, 6).forEach((layer) => {
     outputLayers.push(layer.output);
   });
 
@@ -64,9 +146,10 @@ async function init() {
     reader.onload = () => {
       const dataURL = reader.result;
       output.src = dataURL;
+      sourceImage.src = dataURL;
     };
 
-    output.onload = () => {
+    output.onload = async () => {
       imageTensor = tf.browser.fromPixels(output)
         .expandDims()
         .toFloat()
@@ -77,6 +160,19 @@ async function init() {
 
       const prediction = model.predict(imageTensor).dataSync();
       const activations = activationModel.predict(imageTensor);
+      const options = { score: 0.95, iou: 0.95, topk: 5 };
+      const obPredictions = await odModel.detect(sourceImage, options);
+      console.log(obPredictions);
+
+      ctx.save();
+      // ctx.scale(-1, 1);
+      // ctx.translate(-150, 0);
+      ctx.drawImage(output, 0, 0, ODSIZE, ODSIZE);
+      ctx.restore();
+
+      obPredictions.forEach((obPrediction) => {
+        drawBoundingBoxes(obPrediction);
+      });
 
       // model.layers[0].batchInputShape[1]
       drawActivation(activations, 0, 0, 'plot-activations-one', 150);
@@ -87,10 +183,46 @@ async function init() {
       drawActivation(activations, 5, 0, 'plot-activations-six', 18);
 
       document.getElementById('p-prediction').innerHTML = prediction;
+      // argMax returns a tensor with the arg max index. So,
+      // we need dataSync to convert it to array and indexing to
+      // get the value.
+      const label = tf.argMax(prediction).dataSync()[0];
+      console.log(label);
+      if (label === 0) {
+        console.log('hey its James!');
+      }
+      document.getElementById('p-label').innerHTML = ((label === 0) ? 'James' : 'Jessie');
     };
 
     reader.readAsDataURL(input.files[0]);
   };
+}
+
+function drawBoundingBoxes(prediction) {
+  ctx.font = '20px Arial';
+  const {
+    left, top, width, height,
+  } = prediction.box;
+
+  // Draw the box
+  ctx.strokeStyle = BBCOLOR;
+  ctx.lineWidth = 1;
+  ctx.strokeRect(left, top, width, height);
+
+  // Draw the label background
+  ctx.fillStyle = BBCOLOR;
+  const textWidth = ctx.measureText(prediction.label).width;
+  const textHeight = parseInt(ctx.font, 10);
+
+  // Top left rectangle
+  ctx.fillRect(left, top, textWidth + textHeight, textHeight * 2);
+  // Bottom left rectangle
+  ctx.fillRect(left, top + height - textHeight * 2, textWidth + textHeight, textHeight * 2);
+
+  // Draw labels and score
+  ctx.fillStyle = '#000000';
+  ctx.fillText(prediction.label, left, top + textHeight);
+  ctx.fillText(prediction.score.toFixed(2), left, top + height - textHeight);
 }
 
 function initSliders(layerNumber, cb) {

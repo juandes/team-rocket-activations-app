a18df3634407648ba408208ec434158632c76a43
Juan De Dios Santos <me@juandes.com>
2020-06-19 17:55:29 +1200

    Moved OD model location

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ function setupSliders() {
 }
 
 async function setupModels() {
-  classifierModel = await tf.loadLayersModel('http://127.0.0.1:8080/models/tfjs-3/model.json');
+  classifierModel = await tf.loadLayersModel('http://127.0.0.1:8080/models/tfjs-version/model.json');
   odModel = await tf.automl.loadObjectDetection('models/object-detection/model.json');
 
   const outputLayers = [];
